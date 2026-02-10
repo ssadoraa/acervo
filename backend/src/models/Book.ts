44291e628139b8265d5014bc85b73885744c8267
ssadoraa <isadoraverbicario@hotmail.com>
2026-02-10 12:28:32 -0300

    feature: creating the model for book and enum of category

---
+++ backend/src/models/Book.ts
@@ -0,0 +1,28 @@
+import { Schema, model, Document } from "mongoose";
+import { Category } from "../enums/Category";
+
+interface IBook extends Document {
+    title: string;
+    author: string;
+    isbn: string;
+    category: Category;
+    publisher: string;
+    publicationYear: number;
+    totalQuantity: number;
+    insertAt?: Date;
+}
+
+const bookSchema = new Schema<IBook>(
+  {
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    isbn: { type: String, required: true },
+    category: { type: String, enum: Object.values(Category), required: true },
+    publisher: { type: String, required: true },
+    publicationYear: { type: Number, required: true },
+    totalQuantity: { type: Number, required: true }
+  },
+  { timestamps: { createdAt: "insertAt" } }
+);
+
+export const Book = model<IBook>("Book", bookSchema);
