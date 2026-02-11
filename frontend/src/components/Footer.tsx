13e0f8bfe211641af2937e98ff94477a21baff10
ssadoraa <isadoraverbicario@hotmail.com>
2026-02-11 16:11:55 -0300

    feature: creating footer

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+        <footer className="footer fixed-bottom text-center py-2 bg-dark text-white">
+            <p>&copy; 2026 Acervô. Todos os direitos reservados.</p>
+        </footer>
+
+    );
+}
